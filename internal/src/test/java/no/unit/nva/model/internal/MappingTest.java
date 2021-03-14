06db9628cff73a33795e3f1628db05e330ec8ec9
brinxmat <789709+brinxmat@users.noreply.github.com>
2021-03-14 21:07:24 +0100

    Extract test constants

--- internal/src/test/java/no/unit/nva/model/internal/MappingTest.java
+++ internal/src/test/java/no/unit/nva/model/internal/MappingTest.java
@@ -11,13 +11,17 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
 public class MappingTest {
+
+    public static final String ANY_HTTPS_URI = "https://example.org/resource/123";
+    public static final int EMPTY = 0;
+
     @Test
     void mainTitleIsRoundTrippedBetweenExternalAndInternalModel() {
-        var external = new ResourceDto(URI.create("https://example.org/resource/123"));
+        var external = new ResourceDto(URI.create(ANY_HTTPS_URI));
         var internal = new ResourceDao(external);
         var actual = internal.asDto();
         var javers = JaversBuilder.javers().build();
         var diff = javers.compare(external, actual);
-        assertThat((diff.getChanges().size()), is(equalTo(0)));
+        assertThat((diff.getChanges().size()), is(equalTo(EMPTY)));
     }
 }
